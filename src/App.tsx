1e3df97ecc331d8338bde56f7aa4dd626c1d0f8a
Oleg0009 <42346617+Oleg0009@users.noreply.github.com>
2023-10-20 18:01:22 +0300

    add canvas animation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <main>
+
       <HeroSection />
     </main>
   );
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,41 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import Links from '../base/Links';
 
 
+
 function HeroSection() {
-
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-
-  const draw = (ctx: any) => {
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fill()
-  }
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas && canvas != null) {
-      const context = canvas.getContext('2d');
-      // Perform your canvas operations here using the context
-      context && draw(context)
-    }
-  }, [draw]);
-
 
   return (
     <div>
-      <canvas ref={canvasRef}>
-
-      </canvas>
+      <div id="pt" className="canvas"><canvas id="canvas" width="973" height="1048" ></canvas></div>
       <div className='absolute inset-0 flex flex-col items-center justify-center'>
         <div className='flex items-center justify-center'>
-          <span className='text-white animated-text mr-3 xsm:text-xs'>
+          <span className='text-white animated-text mr-3 '>
             Hello, I'm
           </span>
-          <span className='text-highlight animated-text-2'>
+          <span className='text-highlight animated-text-2 '>
             Oleg Statkevych
           </span>
         </div>
